6560949bcf43507ce8965e92bb33c13e3b127139
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-05 16:49:27 -0500

    got style and most of function complete

--- TheApp/src/App.js
+++ TheApp/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [ balls, setBalls] = useState(0);
+  const [fouls, setFouls] = useState(0);
+
+  if(strikes > 3){
+    setStrikes(0);
+  }
+  if(balls > 4){
+    setBalls(0)
+  }
+  if(fouls > 2){
+    setFouls(2)
+  }
+
+  function theHit(){
+    setStrikes(0);
+    setBalls(0);
+    setFouls(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Stats</h1>
       </header>
+      <div className="the-display label" >
+            <div className="view-stat-title" >Strikes</div>
+            <div className="view-stat-title" >Balls</div>
+            <div className="view-stat-title" >Fouls</div>
+          </div>
+
+          <div className="the-display" >
+            <div className="view-stat" >{strikes}</div>
+            <div className="view-stat" >{balls}</div>
+            <div className="view-stat" >{fouls}</div>
+          </div>
+        <div className="the-dashboard" >
+            <button className="clickity" onClick={() => setStrikes(strikes + 1)}>STRIKES</button>
+            <button className="clickity" onClick={() => setBalls(balls + 1)}>BALLS</button>
+            <button className="clickity" 
+            onClick={() => setFouls(fouls + 1)}
+            onDoubleClick={()=> setFouls(0)}
+            >FOULS</button>
+            <button className="clickity" onClick={theHit}>HIT</button>
+        </div>
     </div>
   );
 }
